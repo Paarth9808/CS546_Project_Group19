f400986f4a7a256c21f26bd5a3de74a84817b07f
Iceman1018 <123434364+Iceman1018@users.noreply.github.com>
2023-04-24 19:22:34 -0400

    commit

--- app.js
+++ app.js
@@ -24,7 +24,9 @@ app.use(rewriteUnsupportedBrowserMethods);
 
 app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
 configRoutes(app);
+
 app.listen(3000, () => {
     console.log("We've now got a server!");
     console.log('Your routes will be running on http://localhost:3000');
--- routes/comment.js
+++ routes/comment.js
@@ -1,41 +1,77 @@
-import axios from "axios";
-import * as fs from "fs";
-import formidable from "formidable";
 import {Router} from 'express';
-import {createComment,deleteComment,updateComment,getCommentById,agreeComment,nonagreeComment,reportComment} from '../data/comment.js'
+import formidable from 'formidable';
+import {v4 as uuidv4} from 'uuid';
+import fs from 'fs';
+import path from 'path';
+import Handlebars from 'handlebars';
+import {createComment,getpartComment} from '../data/comment.js';
 const router = Router();
-
+const commentsource=fs.readFileSync('.\\views\\comment.handlebars');
+const mycomment=Handlebars.compile(commentsource+"");
 router.route('/commenttest').get(async (req, res) => {
     //code here for GET
-    res.render('comments', {Titlename:"Comments test"});
+    const tempcomments=await getpartComment(0,3);
+    for(var i=0;i<tempcomments.length;i++)
+    {
+      tempcomments[i].profilepath='../userfile/profiles/heng.jpg';
+      tempcomments[i].username='Heng';
+    }
+    //console.log(tempcomments);
+    res.render('comments', {Titlename:"Comments test",commentlist:tempcomments});
   });
-router.route("/commenttest").post(async (req, res)=> {
+
+router.route('/commenttest/getmore/:index').get(async (req,res)=>{
+  const start=req.params.index;
+  console.log(start);
+  const newcomment=await getpartComment(Number(start),10);
+  console.log(newcomment);
+  const htmllist=[];
+  for(var i=0;i<newcomment.length;i++)
+  {
+    console.log(mycomment({comment:newcomment[i]}));
+    htmllist.push(mycomment({comment:newcomment[i]}));
+  }
+  res.send(htmllist);
+})
+
+router.route("/commenttest/test").post(async (req, res)=> {
+    console.log(req.body);
     var form = new formidable.IncomingForm();
     form.encoding = 'utf-8';
-    form.uploadDir = "../userfile/comments/";
     form.keepExtensions = true;
-    form.maxFieldsSize = 4 * 1024 * 1024;
+    form.maxFieldsSize = 8 * 1024 * 1024;
+    console.log(form)
     form.parse(req, async (err, fields, files)=> {
-        const userid=fields.userid
-        const gameid=fields.gameid
+        console.log(fields);
+        console.log(files);
         const text=fields.text;
         const pics=[];
-        const picCount=0;
+        var picCount=0;
         for(var filename in files){
+            console.log(filename);
             const file =files[filename];
-            const picName = uuid.v1() + path.extname(file.name);
-            fs.rename(file.path, '..\\userfile\\comments\\' + picName, function (err) {
+            const picName = uuidv4() ;
+            fs.copyFile(file.filepath, '.\\public\\userfile\\comments\\' + picName+'.jpg', function (err) {
                 if (err) {
+                    console.log(err);
                   return res.send({ "error": 403, "message": "image save error！" });
                 }
-                pics.push('../userfile/comments/' + picName);
-                picCount++
             });
+            pics.push('/public/userfile/comments/' + picName+'.jpg');
+            picCount++
         }
         if (picCount === Object.keys(files).length) {
-            res.send({ "picAddrs": pics, "fields": fields });
-            await createComment(userid,gameid,text,pics)
+            //res.send({ "picAddrs": pics, "fields": fields });
+            console.log("transmission finished");
+            console.log("text:"+text+",pict:"+picCount);  
+            console.log(pics);
         }
+        //const userid=req.session.user.userid;
+        //get the gameid from the front end and name it as gameif
+        //const newcomment=await createComment(userid,gameid,text,pics);
+        const newcomment=await createComment("Heng Zhao","Need For Speed",text,pics);
+        //console.log(newcomment);
+        res.send(mycomment({comment:newcomment}));
     });
 });
 export default router;
