25ef526249e0304d539f21aa8193a8a6963fd4cb
Rohit s <rohits@Rohits-MacBook-Air.local>
2023-04-24 21:18:08 -0400

    Revert "Revert "Merge pull request #2 from Paarth9808/development_rohit""
    This reverts commit 3fe7cb6841b39d2503ff6f666784efaefc75fdd3.

--- seed.js
+++ seed.js
@@ -4,10 +4,10 @@ import {gameData} from './data/index.js'
 async function main(){
     const db = await dbConnection();
     try{ 
-        let game=undefined;
-        game=await gameData.createGame('02/02/1970','COD',['Shooter'],'Sample description',['Requirements'],'Teen')
-        await gameData.createGame('02/02/1970')
-        console.log(game)
+        //let game=undefined;
+        //game=await gameData.createGame('02/02/1970','COD',['Shooter'],'Sample description',['Requirements'],'Teen')
+        console.log(await gameData.updateGame('642a12913884b92cf5f2c801','COD',['Shooter'],'Sample description',['Requirements'],'Teen'))
+        //console.log(game)
     }catch(e){
         console.log(e)
     }
